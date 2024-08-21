b3044a5e09eee885974f7af15b3daa156366a383
CZ-RBelo <rui.belo@conceptzoa.com>
2024-08-21 17:26:41 +0000

    Python Module 2 - Part 2

---
+++ Courses/Python/Module2-Part1.py
@@ -0,0 +1,11 @@
+x=10.32
+print(type(x))
+print(x)
+print (isinstance(x, str))
+Number = 10
+print(Number)
+print(float(Number))
+
+SomeNumber = 13.9
+print(SomeNumber)
+print(int(SomeNumber))
---
+++ Courses/Python/Module2-Part2.py
@@ -0,0 +1,22 @@
+first_string = "hello"
+second_string = first_string
+
+print(first_string)
+print(second_string)
+
+first_string = "changed"
+print(first_string)
+print(second_string)
+
+first_name="daVid"
+last_name="bigger"
+full_name = first_name + " " + last_name
+age = 40
+
+print(first_name.title())
+print(first_name.upper())
+print(first_name.lower())
+
+print(full_name.title())
+
+print(full_name.title() + " is " + str(age) + " years old.")
