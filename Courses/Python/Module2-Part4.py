d19eb83c6978881460b76c69d31db347a380a99f
CZ-RBelo <rui.belo@conceptzoa.com>
2024-08-21 17:56:44 +0000

    Python Module 2 - Part 4

---
+++ Courses/Python/Module2-Part4.py
@@ -0,0 +1,21 @@
+# Practice Time
+
+# Integer variable with value of 15
+var_int = 15
+
+# Float variable with value of 6.36272
+var_float = 6.36272
+
+# Boolean variable with value of true
+var_boolean = 10 > 1
+
+# String variable that concatenate them together
+var_string = "Int var:" + str(var_int) + " | Float var: " + str(var_float) + " | Boolean var: " + str(var_boolean)
+
+# Print the variable 
+print (var_string)
+
+# Print variables Types
+print (type(var_int))
+print (type(var_float))
+print (type(var_string))
