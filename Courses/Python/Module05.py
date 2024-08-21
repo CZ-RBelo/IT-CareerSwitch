c62abaa78db45152ae8a2ec4881a10e133a875b0
CZ-RBelo <rui.belo@conceptzoa.com>
2024-08-21 19:03:58 +0000

    Python Module 5 - Creating Lists

---
+++ Courses/Python/Module05.py
@@ -0,0 +1,8 @@
+# Creating Lists
+
+employees = ["Sara", "Tammy", "Debbie", "John", "Carrie"]
+
+print(employees[0])
+print(employees[2])
+
+print(employees[1] + "Data type: " + str(type(employees[1])))
