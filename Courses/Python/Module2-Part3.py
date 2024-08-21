102dae8a9b00504c43096370d035c1ef97c152c4
CZ-RBelo <rui.belo@conceptzoa.com>
2024-08-21 17:44:20 +0000

    Python Module 2 - Part 3

---
+++ Courses/Python/Module2-Part3.py
@@ -0,0 +1,11 @@
+# More Strings
+
+game = "  baseball "
+print(game.strip()) # strip() removes the white space from both ends
+print(game.lstrip()) # lstrip() removes the white space from the left
+print(game.rstrip()) # rstrip() removes the white space from the right
+
+# Boolean
+
+the_Boolean = 10 > 20
+print(the_Boolean) # prints False
