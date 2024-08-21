eeb4775594e079e88a8d853af0b43c68c65e160a
CZ-RBelo <rui.belo@conceptzoa.com>
2024-08-21 18:47:32 +0000

    Python Module 4 - Converting types in Python

---
+++ Courses/Python/Module4.py
@@ -0,0 +1,5 @@
+# Converting types in Python
+
+print(int(321.88)) # int() convert float to int
+print(float(12)) # float() convert int to float
+print("Converting to string: " + str(99)) # str() convert to string
