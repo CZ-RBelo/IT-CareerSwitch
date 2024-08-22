4e56529d8633c35df039f68517b8c354a0f6c361
CZ-RBelo <rui.belo@conceptzoa.com>
2024-08-22 17:35:53 +0000

    Pytohn Module 9 - Part 2 - Working with operators

--- Courses/Python/Module09-Part2.py
+++ Courses/Python/Module09-Part2.py
@@ -1,2 +1,45 @@
 # Working with operators
 
+""" 
+Comparison Operators
+
+== Equal
+!= Not equel
+> Greater Than
+< Less Than
+>= Greater Than or Equal To
+<= Less Than or Equal To
+"""
+
+# Greater Than or Equal To Example
+print("\n-> Greater Than or Equal To")
+
+x = 10
+y = 12
+
+print(x >= y)
+
+x = 12
+y = 10
+
+print(x >= y)
+
+""" 
+Logical Operators
+
+and - True if both statements are true - Example: x < 3 and x < 6
+or - True if either statements are true - Example: x < 3 or y > 4
+not - Reverses the state of the operand - Example: not(x < 3 and x < 6)
+
+"""
+
+print("\n-> AND")
+a = 5
+print (a > 2 and a < 7)
+print (a > 2 and a < 5)
+
+
+print("\n-> NOT")
+a = 4
+print (not(a > 2 and a < 7))
+print (not(a > 2 and a < 4))
