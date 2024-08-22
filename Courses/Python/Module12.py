c2d50ef5ae7b426b1aa160454c1d6fa97d310713
CZ-RBelo <rui.belo@conceptzoa.com>
2024-08-22 19:11:22 +0000

    Pytohn Module 12 - Working with FOR Loops

---
+++ Courses/Python/Module12.py
@@ -0,0 +1,23 @@
+# Working with FOR Loops
+
+print("\n-> FOR Loops Statement")
+
+colors = ['blue', 'red', 'pink', 'green']
+
+print(colors)
+for x in colors:
+    print(x)
+
+print("\n-> FOR RANGE() Loops Statement")
+
+print("\n-> Print a RANGE start 0 stop at 9")
+for a in range(10):
+    print(a)
+
+print("\n-> Print a RANGE start at 10 stop 34")
+for x in range(10, 35):
+    print(x)
+
+print("\n-> Print a RANGE start at 0 stop 98 step 2")
+for x in range(0, 100, 2):
+    print(x)
