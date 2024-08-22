fd2582c6e2b502310fb6233c2b235b786d34b35a
CZ-RBelo <rui.belo@conceptzoa.com>
2024-08-22 17:12:13 +0000

    Pytohn Module 9 - Part 1 - Working with operators

---
+++ Courses/Python/Module09-Part1.py
@@ -0,0 +1,36 @@
+# Working with operators
+
+
+""" 
+Arithmetic Operators
+
++
+-
+*
+/
+**
+"""
+
+# Exponents Example
+print("\n-> Exponents Example")
+a = 4
+b = 2
+print(a**b)
+
+
+""" 
+Assignment Operators
+
+=
++=
+-=
+*=
+/=
+**= 
+"""
+
+print("\n-> ADD AND Example")
+a = 4
+b = 2
+a += b
+print(a)
